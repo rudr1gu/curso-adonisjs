cd384016adaddf8377a533ce3fac3415c83b3977
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-08-02 16:09:43 -0300

    feat: Add image upload functionality to MomentsController

--- app/controllers/moments_controller.ts
+++ app/controllers/moments_controller.ts
@@ -1,11 +1,29 @@
+import { v4 as uuidv4 } from 'uuid';
 import type { HttpContext } from '@adonisjs/core/http'
 import Moment from '#models/moment'
+import app from '@adonisjs/core/services/app'
+
+
+
 
 export default class MomentsController {
+
+  // define as opções de validação nesse caso é uma imagem de até 2mb
+  private validationOptions = {
+    types: ['image'],
+    size: '2mb',
+  }
+
   /**
    * Display a list of resource
    */
-  // async index({}: HttpContext) {}
+  async index() {
+    const moments = await Moment.all()
+
+    return {
+      data: moments,
+    }
+  }
 
   /**
    * Display form to create a new record
@@ -15,17 +33,38 @@ export default class MomentsController {
   /**
    * Handle form submission for the create action
    */
+
+
   async store({ request, response }: HttpContext) {
     // faz uma requisição para o body 'corpo' da requisição
     const body = request.body()
 
+    // verifica se a requisição tem um arquivo de imagem
+    const image = request.file('image', this.validationOptions)
+
+    // se tiver uma imagem, salva ela no diretório tmp/uploads
+    if(image) {
+      // gera um nome único para a imagem
+      const imageName = `${uuidv4()}.${image.extname}`
+
+      // move a imagem para o diretório tmp/uploads
+      await image.move(app.tmpPath('uploads'), {
+        name: imageName,
+      })
+
+      // se a imagem não for movida, retorna um erro
+      body.image = imageName
+    }
+
+
     // cria um novo registro no banco de dados
     const moment = await Moment.create(body)
 
+    // se a requisição for feita via API, retorna um JSON com o registro criado
     response.status(201).json(moment)
 
     return {
-      message: 'Moment created successfully!',
+      message: 'Momento criado com sucesso!',
       data: moment,
     }
     
@@ -34,7 +73,13 @@ export default class MomentsController {
   /**
    * Show individual record
    */
-  // async show({ params }: HttpContext) {}
+  async show({ params }: HttpContext) {
+    const moment = await Moment.findOrFail(params.id)
+
+    return {
+      data: moment,
+    }
+  }
 
   /**
    * Edit individual record
